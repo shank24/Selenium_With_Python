13a5c9f928e81d9fe7c1af1464d3ed0d6337e360
shankkalra <shankkalra@gmail.com>
2021-08-10 12:09:29 +0530

    Regex Locator

--- Basic_Selenium_Actions/Locator.py
+++ Basic_Selenium_Actions/Locator.py
@@ -2,6 +2,14 @@ from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver')
 driver.get("https://rahulshettyacademy.com/angularpractice/")
-name = driver.find_element_by_name("name")
-name.send_keys("Shank")
+#Textbox
+#driver.find_element_by_name("name").send_keys("Shank")
+driver.find_element_by_css_selector("input[name='name']").send_keys("Shank")
+#Checkbox
+driver.find_element_by_id("exampleCheck1").click()
+driver.find_element_by_xpath(".//input[@type='submit']").click()
+print(driver.find_element_by_class_name("alert-success").text)
+print(driver.find_element_by_class_name([class*='alert-success']).text)
+# Regular Expression
+#//*[contains(@class,'alert-success')]
 driver.close()
