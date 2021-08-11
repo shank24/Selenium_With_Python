107339eaec24a594db7db1cad829b2ea26325fa3
shankkalra <shankkalra@gmail.com>
2021-08-11 20:07:56 +0530

    Locator Update

--- Basic_Selenium_Actions/Locator.py
+++ Basic_Selenium_Actions/Locator.py
@@ -9,7 +9,11 @@ driver.find_element_by_css_selector("input[name='name']").send_keys("Shank")
 driver.find_element_by_id("exampleCheck1").click()
 driver.find_element_by_xpath(".//input[@type='submit']").click()
 print(driver.find_element_by_class_name("alert-success").text)
-print(driver.find_element_by_class_name([class*='alert-success']).text)
-# Regular Expression
+
+#Regular Expression
+#print(driver.find_element_by_class_name([class*='alert-success']).text)
 #//*[contains(@class,'alert-success')]
+
+#Tag Name With css
+#input#username
 driver.close()
