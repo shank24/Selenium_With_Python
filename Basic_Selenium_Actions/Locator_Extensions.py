98ada0fee9760e163f947c242946ab2a1a17b46b
shankkalra <shankkalra@gmail.com>
2021-08-13 19:51:14 +0530

    Diff Locator Demo

---
+++ Basic_Selenium_Actions/Locator_Extensions.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver')
+driver.get("https://login.salesforce.com/")
+driver.find_element_by_css_selector("#username").send_keys("Shank")
+driver.find_element_by_css_selector(".password").send_keys("12345")
+driver.find_element_by_css_selector(".password").clear()
+driver.find_element_by_link_text("Forgot Your Password?").click()
+driver.close()
+
+
+
