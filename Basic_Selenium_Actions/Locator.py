788c96d69f7dd554c969eb8dc540f1f64d85a786
shankkalra <shankkalra@gmail.com>
2021-08-20 19:45:22 +0530

    Select Class

--- Basic_Selenium_Actions/Locator.py
+++ Basic_Selenium_Actions/Locator.py
@@ -1,4 +1,6 @@
+
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver')
 driver.get("https://rahulshettyacademy.com/angularpractice/")
@@ -7,8 +9,16 @@ driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.find_element_by_css_selector("input[name='name']").send_keys("Shank")
 #Checkbox
 driver.find_element_by_id("exampleCheck1").click()
+
+#Select Class Demo
+
+dropdown = Select(driver.find_element_by_id("exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+dropdown.select_by_index(0)
+
 driver.find_element_by_xpath(".//input[@type='submit']").click()
 print(driver.find_element_by_class_name("alert-success").text)
+
 
 #Regular Expression
 #print(driver.find_element_by_class_name([class*='alert-success']).text)
--- Basic_Selenium_Actions/Locator_Extensions.py
+++ Basic_Selenium_Actions/Locator_Extensions.py
@@ -5,6 +5,7 @@ driver.get("https://login.salesforce.com/")
 driver.find_element_by_css_selector("#username").send_keys("Shank")
 driver.find_element_by_css_selector(".password").send_keys("12345")
 driver.find_element_by_css_selector(".password").clear()
+s
 driver.find_element_by_link_text("Forgot Your Password?").click()
 driver.find_element_by_xpath("//input[@name='cancel']").click()
 print(driver.find_element_by_xpath("//div[@id='usernamegroup']/label[text()='Username']").text)
