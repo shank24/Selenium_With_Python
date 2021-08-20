7e597ba3729ae9628b7046f771b62e3b11d05ae5
shankkalra <shankkalra@gmail.com>
2021-08-20 20:26:33 +0530

    Dynamic Dropdown

---
+++ Basic_Selenium_Actions/Dynamic_DropDown.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver')
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
+driver.find_element_by_id("autosuggest").send_keys("ind")
+time.sleep(2)
+
+countries  = driver.find_elements_by_css_selector("li[class='ui-menu-item'] a")
+print(len(countries))
+
+for country in countries:
+    if country.text == "India":
+        country.click()
+        time.sleep(2)
+        break
+
+print(driver.find_element_by_id("autosuggest").get_attribute('value'))
+driver.close()
