fa789c8156526b1e4a8c58bcc803abec4cc89a1e
shankkalra <shankkalra@gmail.com>
2021-08-22 11:09:56 +0530

    Assert Text

--- Basic_Selenium_Actions/JavaScript_Handlers.py
+++ Basic_Selenium_Actions/JavaScript_Handlers.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 
+
+
 driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver')
 driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+
+validateText = 'Option3'
 
 driver.find_element_by_css_selector('#name').send_keys('Option3')
 driver.find_element_by_id("alertbtn").click()
 alert = driver.switch_to_alert()
-print(alert.text)
+print(validateText in alert.text)
 alert.accept()
 
 driver.close()
