d61896571352c525f5488b35bd85b3e22814f77b
shankkalra <shankkalra@gmail.com>
2021-08-22 10:59:27 +0530

    RadioButton Condition

--- Basic_Selenium_Actions/UI_Controls.py
+++ Basic_Selenium_Actions/UI_Controls.py
@@ -5,14 +5,21 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver')
 driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
 
+#Checkbox Code
 checkbox = driver.find_elements_by_xpath("//input[@type='checkbox']")
 
-print(len(checkbox))
+print("No. of Checkboxes",len(checkbox))
 
 for chBox in checkbox:
     print(chBox.get_attribute("value"))
     if chBox.text == 'option2':
         chBox.click()
         assert chBox.is_selected()
+
+
+#RadioButton Code
+radioButton = driver.find_elements_by_name("radioButton")
+radioButton[2].click()
+assert radioButton[2].is_selected()
 
 driver.close()
