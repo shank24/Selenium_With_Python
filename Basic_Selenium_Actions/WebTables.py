4e9be5b915260df66c9e1fe722dcb61102f31844
shankkalra <shankkalra@gmail.com>
2021-09-05 20:38:32 +0530

    WebTables

--- Basic_Selenium_Actions/WebTables.py
+++ Basic_Selenium_Actions/WebTables.py
@@ -6,10 +6,16 @@ driver.get("https://www.techlistic.com/p/demo-selenium-practice.html")
 
 driver.maximize_window()
 value = driver.find_elements_by_xpath("//tr/td[5]")
+print(type(value))
 
 sum = 0
 for val in value:
     sum = sum + int(val.text)
 
 print(sum)
+
+# for i in range(1,6):
+#     elements = driver.find_elements_by_xpath('//tr/td[1]')
+#     print(elements.text)
+#
 driver.close()
