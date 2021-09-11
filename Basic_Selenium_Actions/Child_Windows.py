f347ad23b5ee6818e058d4fcba4b673f21d096cb
shankkalra <shankkalra@gmail.com>
2021-09-11 22:42:17 +0530

    Child Win

--- Basic_Selenium_Actions/Child_Windows.py
+++ Basic_Selenium_Actions/Child_Windows.py
@@ -9,9 +9,10 @@ driver.find_element_by_xpath(".//a[text()='Click Here']").click()
 #Switching to Child Window
 getWindows = driver.window_handles
 driver.switch_to_window(getWindows[1])
-print(driver.find_element_by_tag_name("h3").text)
+assert "New Window" == driver.find_element_by_tag_name("h3").text
+driver.close()
 #Switching to Parent Window
 #driver.close(getWindows[1])
 driver.switch_to_window(getWindows[0])
-print(driver.find_element_by_tag_name("h3").text)
-driver.quit()
+assert "Opening a new window" == driver.find_element_by_tag_name("h3").text
+driver.close()
