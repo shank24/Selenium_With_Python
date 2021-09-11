9c9e0514f117b96a06f1ffbec601c3e513645421
shankkalra <shankkalra@gmail.com>
2021-09-11 22:03:54 +0530

    Handling Windows

--- Basic_Selenium_Actions/Child_Windows.py
+++ Basic_Selenium_Actions/Child_Windows.py
@@ -6,5 +6,12 @@ driver.get("https://the-internet.herokuapp.com/windows")
 driver.maximize_window()
 
 driver.find_element_by_xpath(".//a[text()='Click Here']").click()
+#Switching to Child Window
+getWindows = driver.window_handles
+driver.switch_to_window(getWindows[1])
 print(driver.find_element_by_tag_name("h3").text)
-driver.close()
+#Switching to Parent Window
+#driver.close(getWindows[1])
+driver.switch_to_window(getWindows[0])
+print(driver.find_element_by_tag_name("h3").text)
+driver.quit()
