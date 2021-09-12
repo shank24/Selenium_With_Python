33138355786741486c3e9ebd74a865f555574bd5
shankkalra <shankkalra@gmail.com>
2021-09-12 10:51:25 +0530

    IFrames

--- Basic_Selenium_Actions/Frame_Demo.py
+++ Basic_Selenium_Actions/Frame_Demo.py
@@ -9,6 +9,6 @@ driver.maximize_window()
 driver.switch_to.frame("mce_0_ifr")
 driver.find_element_by_css_selector("#tinymce").clear()
 driver.find_element_by_css_selector("#tinymce").send_keys("Automate")
-
-
+driver.switch_to_default_content()
+print(driver.find_element_by_tag_name("h3").text)
 driver.close()
