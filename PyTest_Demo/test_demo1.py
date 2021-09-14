ba4278af1dead68fff8572979f5dc9e0a977c8c2
shankkalra <shankkalra@gmail.com>
2021-09-14 11:29:25 +0530

    PyTest Demo

---
+++ PyTest_Demo/test_demo1.py
@@ -0,0 +1,7 @@
+#Coding Convention - start with test_ or end with _test.
+#Pytest method should start with test
+#Code should be wrapped in a method.
+
+def test_first_program():
+    print("Hello")
+
