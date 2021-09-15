4ded128cca394c0ee4ea25d0a6fc1aa8a1f735d2
shankkalra <shankkalra@gmail.com>
2021-09-15 11:27:28 +0530

    Tagging -m smoke

--- PyTest_Demo/test_demo1.py
+++ PyTest_Demo/test_demo1.py
@@ -1,7 +1,10 @@
 #Coding Convention - start with test_ or end with _test.
 #Pytest method should start with test
 #Code should be wrapped in a method.
+import pytest
 
+
+@pytest.mark.smoke
 def test_first_program():
     print("Hello")
 
--- PyTest_Demo/test_demo2.py
+++ PyTest_Demo/test_demo2.py
@@ -1,11 +1,15 @@
-#Coding Convention - start with test_ or end with _test.
-#Pytest method should start with test
-#Code should be wrapped in a method.
-#-k for Regex (Method Name exexution)
-#-s logs in output
-#-v for verbose (More Info Metadata)
+# Coding Convention - start with test_ or end with _test.
+# Pytest method should start with test
+# Code should be wrapped in a method.
+# -k for Regex (Method Name exexution)
+# -s logs in output
+# -v for verbose (More Info Metadata)
+# -m tagname - py.test -m smoke -v -s
 # Running specific file with py.test filenames
+import pytest
 
+
+@pytest.mark.smoke
 def test_first_program():
     msg = "Hello World"
     assert msg == "Hello World", "Strings do not match"
@@ -14,9 +18,4 @@ def test_first_program():
 def test_second_Credit():
     a = 4
     b = 6
-    assert a+2 == 6, "Addition not matching"
-
-
-
-
-
+    assert a + 2 == 6, "Addition not matching"
