ed949296154cf7603cca72028da3e880b95f4817
shankkalra <shankkalra@gmail.com>
2021-09-15 23:24:50 +0530

    FixtureS

--- PyTest_Demo/conftest.py
+++ PyTest_Demo/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 
-#Scope =Class defines the setup execution only before class
+
+# Scope =Class defines the setup execution only before class
 
 @pytest.fixture(scope="class")
 def setup():
     print("Init Block")
-    #Post Init and test_Fixture, Tear down ll run.
-    #yield
-    #print("Tear Down")
+    # Post Init and test_Fixture, Tear down ll run.
+    # yield
+    # print("Tear Down")
