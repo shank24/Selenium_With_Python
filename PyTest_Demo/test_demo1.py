cdacb8a01ea4c62f32d37ccb48eb19ca8ed530e5
shankkalra <shankkalra@gmail.com>
2021-09-20 23:41:04 +0530

    Notes

--- PyTest_Demo/test_demo1.py
+++ PyTest_Demo/test_demo1.py
@@ -1,6 +1,11 @@
 #Coding Convention - start with test_ or end with _test.
 #Pytest method should start with test
 #Code should be wrapped in a method.
+#When you define fixture scope to class,
+# it will run once before class is initiated and at the end.
+#Data Driven & Parameterization can be done
+# with return statements in tuple format.
+
 import pytest
 
 
