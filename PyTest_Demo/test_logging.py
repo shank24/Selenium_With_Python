f0855727b7cae7a4ec1365552cfc9d5a17334602
shankkalra <shankkalra@gmail.com>
2021-09-24 21:28:40 +0530

    Logger

---
+++ PyTest_Demo/test_logging.py
@@ -0,0 +1,19 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+#file handler object is required in addHandler
+
+fileHandler = logging.FileHandler('logfile.log')
+formatter = logging.Formatter("%(asctime)s :%(levelname)s :%(name)s :%(message)s :%(lineno)s")
+
+fileHandler.setFormatter(formatter)
+
+logger.addHandler(fileHandler)
+
+logger.setLevel(logging.INFO)
+logger.debug("A debug statement")
+logger.info("A Information ")
+logger.warning("Warning")
+logger.critical("Critical")
+logger.error("Error !!")
