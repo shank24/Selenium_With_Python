cd172c552ba93f383aac76236cad45570237c61f
shankkalra <shankkalra@gmail.com>
2021-09-25 18:55:26 +0530

    Browser_Add

--- Selenium_Framework/tests/conftest.py
+++ Selenium_Framework/tests/conftest.py
@@ -2,16 +2,28 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
+
 @pytest.fixture(scope="class")
-def setUp(request,):
-    chrome_Options = webdriver.ChromeOptions()
-    chrome_Options.add_argument("--start-maximized")
-    # chrome_Options.add_argument("headless")
-    chrome_Options.add_argument("--ignore-certificate-errors")
-    driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver', options=chrome_Options)
+def setUp(request):
+    browser_name=request.config.getoption("browser_name")
+    #Chrome Invoke
+    if browser_name == "chrome":
+        chrome_Options = webdriver.ChromeOptions()
+        #chrome_Options.add_argument("--start-maximized")
+        # chrome_Options.add_argument("headless")
+        chrome_Options.add_argument("--ignore-certificate-errors")
+        driver = webdriver.Chrome(executable_path='/home/shanky/PycharmProjects/chromedriver', options=chrome_Options)
+    #Firefox Invoke
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(executable_path='/home/shanky/PycharmProjects/geckodriver')
+
     driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.close()
-
-
