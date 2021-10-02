e330e7e6c309908c5cff3230fe6d556d5cd107e1
shankkalra <shankkalra@gmail.com>
2021-10-02 23:01:31 +0530

    Code Refactor

--- Selenium_Framework/tests/test_HomePage.py
+++ Selenium_Framework/tests/test_HomePage.py
@@ -10,11 +10,8 @@ class TestHomePage(BC):
     def test_formSubmission(self):
         homePage = HomePage(self.driver)
         homePage.getName().send_keys("Shank")
-        #driver.find_element_by_css_selector("input[name='name']").send_keys("Shank")
         homePage.getEmail().send_keys("Karan@gmail.com")
-        #driver.find_element_by_name("email").send_keys("Karan@gmail.com")
         homePage.getCheckBox().click()
-        #driver.find_element_by_id("exampleCheck1").click()
 
         # Select Class Demo
         dropdown = Select(homePage.getGender())
@@ -22,9 +19,7 @@ class TestHomePage(BC):
         dropdown.select_by_index(0)
 
         homePage.submitForm().click()
-        #driver.find_element_by_xpath(".//input[@type='submit']").click()
         message = homePage.getMessage().text
-        # message = driver.find_element_by_class_name("alert-success").text
 
         assert "Success" in message
 
