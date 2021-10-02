78f09aaa9bef2a9a92f20fa2b5f347fc57ebaf60
shankkalra <shankkalra@gmail.com>
2021-10-02 23:08:55 +0530

    Base class

--- Selenium_Framework/utilities/BaseClass.py
+++ Selenium_Framework/utilities/BaseClass.py
@@ -2,6 +2,7 @@
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -10,3 +11,7 @@ class BaseClass:
 
     def verifyLinkPresence(self, textToVerify):
         element = WebDriverWait(self.driver, 7).until(EC.presence_of_element_located((By.LINK_TEXT, textToVerify)))
+
+    def selectOptionByText(self,locator, textToSelect):
+        selectElem = Select(locator)
+        selectElem.select_by_visible_text(textToSelect)
