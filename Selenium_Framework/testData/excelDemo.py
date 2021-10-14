cf07f7e935ec7e9eb1458efc9264691d9e579111
shankkalra <shankkalra@gmail.com>
2021-10-14 18:43:12 +0530

    Iterating_Loop

--- Selenium_Framework/testData/excelDemo.py
+++ Selenium_Framework/testData/excelDemo.py
@@ -2,6 +2,17 @@ import openpyxl
 
 book = openpyxl.load_workbook("/home/shanky/PycharmProjects/Python_Testing_Selenium/Selenium_Framework/PythonDemo.xlsx")
 sheet = book.active
-cell = sheet.cell(row=2 , column=2)
+cell = sheet.cell(row=1 , column=2)
 print(cell.value)
+sheet.cell(row=2 , column=2).value = "Shank"
 
+print(sheet.cell(row=2 , column=2).value)
+
+print(sheet.max_row)
+print(sheet.max_column)
+
+print (sheet['A5'].value)
+
+for i in range(1, sheet.max_row+1):
+    for j in range(1, sheet.max_column+1):
+        print(sheet.cell(row=i, column=j).value)
