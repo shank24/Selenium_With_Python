3c92dab2ddbaad90a3a2da11f8789a726a93db26
shankkalra <shankkalra@gmail.com>
2021-10-16 12:58:24 +0530

    Added Excel Util

--- Selenium_Framework/testData/excelDemo.py
+++ Selenium_Framework/testData/excelDemo.py
@@ -2,6 +2,10 @@ import openpyxl
 
 book = openpyxl.load_workbook("/home/shanky/PycharmProjects/Python_Testing_Selenium/Selenium_Framework/PythonDemo.xlsx")
 sheet = book.active
+#initializing Empty Dictionary
+Dict = {}
+
+
 cell = sheet.cell(row=1 , column=2)
 print(cell.value)
 sheet.cell(row=2 , column=2).value = "Shank"
@@ -15,5 +19,8 @@ print (sheet['A5'].value)
 
 for i in range(1, sheet.max_row+1):  #Rows
     if sheet.cell(row=i, column=1).value == "Test2":
-        for j in range(1, sheet.max_column+1):  #Columns
-            print(sheet.cell(row=i, column=j).value)
+        for j in range(2, sheet.max_column+1):  #Columns
+            Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+
+
+        print(Dict)
